5030a3ad9ce5b81eb02ce63af7f82d312f870489
KostyHvlaei <kostya_khvalei@outlook.com>
2022-11-08 16:19:49 +0300

    Add IFridgeProductsService and FridgeProductsService

---
+++ ClientMVC/Services/FridgeProductsService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ClientMVC.Services
+{
+	public class FridgeProductsService
+	{
+		private readonly HttpClient _client;
+		public const string BasePath = "/api/fridges";
+
+		public FridgeProductsService(HttpClient client)
+		{
+			_client = client;
+		}
+	}
+}
